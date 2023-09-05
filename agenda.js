c03c2ccd0f1cdf997521b60ec60cbc38701c99ea
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-09-05 12:19:17 -0300

    agenda commit

---
+++ agenda.js
@@ -0,0 +1,35 @@
+"use strict";
+class Evento {
+    constructor(nome, data, horario) {
+        this.nome = nome;
+        this.data = data;
+        this.horario = horario;
+    }
+}
+class Agenda {
+    constructor() {
+        this.evento = [];
+    }
+    adicionarEvento(evento) {
+        this.evento.push(evento);
+    }
+    visualizar() {
+        for (let i = 0; i <= this.evento.length; i++) {
+            ;
+            console.log(this.evento[i].nome + " na data " + this.evento[i].data + " as " + this.evento);
+        }
+        ;
+    }
+    remover(evento) {
+        this.evento = this.evento.filter((item) => item.nome !== evento.nome);
+    }
+}
+const evento1 = new Evento(" Halloween do Vini ", "20/09", "20h");
+const evento2 = new Evento(" Farofa do Davi", "21/09", "21h");
+const evento3 = new Evento(" Higor Cristo Fest ", "22/09", "22h");
+const minhaAgenda = new Agenda();
+minhaAgenda.adicionarEvento(evento1);
+minhaAgenda.adicionarEvento(evento2);
+minhaAgenda.adicionarEvento(evento2);
+console.log(minhaAgenda.visualizar());
+console.log(minhaAgenda.remover(evento1));
---
+++ agenda.ts
@@ -0,0 +1,60 @@
+class Evento{
+    nome: string;
+    data: string;
+    horario: string;
+
+    constructor(nome:string, data: string, horario:string) {
+        this.nome = nome;
+        this.data = data;
+        this.horario = horario;
+    }
+
+
+}
+
+
+class Agenda{
+    private evento: Evento[] = [];
+   
+
+
+
+    constructor(){
+
+    }
+
+
+    adicionarEvento(evento: Evento){
+
+        this.evento.push(evento);
+
+    }
+
+    visualizar(){
+        for (let i = 0; i<= this.evento.length; i++){;
+            console.log(this.evento[i].nome + " na data " + this.evento[i].data + " as " + this.evento);
+        };
+
+    }
+
+
+    remover(evento: Evento){
+        this.evento = this.evento.filter((item) => item.nome !== evento.nome);
+    }
+
+
+
+}
+
+const evento1 = new Evento(" Halloween do Vini ", "20/09","20h" );
+const evento2= new Evento(" Farofa do Davi", "21/09","21h" );
+const evento3 = new Evento(" Higor Cristo Fest ", "22/09","22h" );
+
+const minhaAgenda = new Agenda();
+minhaAgenda.adicionarEvento(evento1)
+minhaAgenda.adicionarEvento(evento2)
+minhaAgenda.adicionarEvento(evento2)
+console.log(minhaAgenda.visualizar());
+console.log(minhaAgenda.remover(evento1));
+
+
